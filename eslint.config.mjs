058575b50b3b1077826f0b0d1e87fa6d15a9a1b8
Thephaseless <kukubaorch@gmail.com>
2024-10-10 20:50:09 +0000

    add linters and configure env

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  }
+);
