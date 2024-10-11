d34f30c92a062d4172884ba5a00dcf8876777f30
Thephaseless <kukubaorch@gmail.com>
2024-10-11 14:49:12 +0000

    user tile and tweaks

--- src/app/components/user_tile/user_tile.component.ts
+++ src/app/components/user_tile/user_tile.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-tile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './user_tile.component.html',
   styleUrl: './user_tile.component.scss',
 })
