0e3599ce590331ad32c2630859f75aa5fc8d2495
Thephaseless <kukubaorch@gmail.com>
2024-10-11 12:38:22 +0000

    join game component

--- src/app/components/join_tile/join_tile.component.ts
+++ src/app/components/join_tile/join_tile.component.ts
@@ -1,11 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-join-tile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
   templateUrl: './join_tile.component.html',
   styleUrl: './join_tile.component.scss',
 })
-export class JoinTileComponent {}
+export class JoinTileComponent {
+  public formControl = new FormControl<string>('');
+}
