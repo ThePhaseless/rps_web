948dce292b5c9d4a6a9e28aa7aaedfc3366d35c0
Thephaseless <kukubaorch@gmail.com>
2024-10-12 18:40:15 +0000

    login fixes

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,25 +6,23 @@ import { DefaultService } from '../../../api';
   providedIn: 'root',
 })
 export class UserService {
-  google_user!: WritableSignal<SocialUser>;
+  google_user!: WritableSignal<SocialUser | null>;
   loggedIn!: WritableSignal<boolean>;
 
   constructor(
     private authService: SocialAuthService,
     private apiService: DefaultService
   ) {
-    this.authService.authState.subscribe((user) => {
-      this.google_user = signal<SocialUser>(user);
-      this.loggedIn = signal<boolean>(user != null);
-
-      if (user != null) {
+    this.authService.authState.subscribe({
+      next: (user) => {
+        this.google_user = signal<SocialUser | null>(user);
+        this.loggedIn = signal<boolean>(user != null);
         this.apiService.loginLoginGet(user.idToken).subscribe();
-      }
+      },
     });
   }
 
   logout() {
     this.authService.signOut();
-    this.loggedIn.set(false);
   }
 }
