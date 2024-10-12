2a31098d0479ea470fb8ee7d7e0fc15851daa0cb
Thephaseless <kukubaorch@gmail.com>
2024-10-12 18:48:33 +0000

    more logout logic

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -25,7 +25,10 @@ export class UserService {
   }
 
   logout() {
-    this.authService.signOut();
-    this.notesService.notes.set([]);
+    this.apiService.logoutLogoutGet().subscribe(() => {
+      this.notesService.notes.set([]);
+      this.authService.signOut();
+      this.loggedIn.set(false);
+    });
   }
 }
