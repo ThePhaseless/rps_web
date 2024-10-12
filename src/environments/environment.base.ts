a10dcf292b7220619dfe4a7538dccbe5292e2a10
Thephaseless <kukubaorch@gmail.com>
2024-10-12 18:10:38 +0000

    use public api

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_BASE_PATH: `http://localhost:8000`,
+  API_BASE_PATH: `https://myapi.nerine.dev`,
   cliend_id:
     '636831366987-34qnitude6pg8d73u1njubuu05c8lo8o.apps.googleusercontent.com',
 };
